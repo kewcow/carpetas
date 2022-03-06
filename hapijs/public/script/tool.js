77d63b3130743b52c5802bfd3449d99ee5bb4577
kewcow <kevjhoss@gmail.com>
2022-03-06 00:54:28 -0300

    martes 6 marzo

--- hapijs/public/script/tool.js
+++ hapijs/public/script/tool.js
@@ -1,22 +1,85 @@
 "use strict";
-class persons {
-  name;
-  age;
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+// Setup canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+// Random number
+const random = (min, max) => {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+};
+// Color random
+const randomRGB = () => {
+  const rgb = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+  return rgb;
+};
+// Balls
+class ball {
+  x;
+  y;
+  velX;
+  velY;
+  color;
+  size;
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
-  hello() {
-    console.log(`I am ${this.name} and ${this.age} years`);
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  collisionDetect() {
+    for (const boll of balls) {
+      if (!(this == boll)) {
+        const dx = this.x - boll.x;
+        const dy = this.y - boll.x;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.size + boll.size) {
+          boll.color = this.color = randomRGB();
+        }
+      }
+    }
   }
 }
-class users extends persons {
-  constructor(name, age) {
-    super(name, age);
-  }
-  bay() {
-    console.log(`bay ${this.name} of ${this.age} years`);
-  }
+;
+//Animate balls
+const balls = [];
+while (balls.length < 5) {
+  const size = random(10, 20);
+  const boll = new ball(random(0 + size, width - size), random(0 + size, height - size), random(-7, 7), random(-7, 7), randomRGB(), size);
+  balls.push(boll);
 }
-const a = new persons('jhosse', 22);
-const b = new users('kevin', 20);
+const loop = () => {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+  for (const boll of balls) {
+    boll.draw();
+    boll.update();
+    boll.collisionDetect();
+  }
+  requestAnimationFrame(loop);
+};
--- hapijs/src/tool.ts
+++ hapijs/src/tool.ts
@@ -1,28 +1,105 @@
-interface obj{
-  name: string;
-  age: number;
+// Setup canvas
+const canvas: HTMLCanvasElement = document.querySelector('canvas') as HTMLCanvasElement;
+const ctx: any = canvas.getContext('2d');
+const width: number = canvas.width = window.innerWidth;
+const height: number = canvas.height = window.innerHeight;
+
+// Random number
+const random = (min: number, max: number) => {
+  const num: number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
 }
 
-class persons {
-  name: string;
-  age: number;
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-  hello() {
-    console.log(`I am ${this.name} and ${this.age} years`);
-  }
+// Color random
+const randomRGB = () => {
+  const rgb: string = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+  return rgb;
 }
 
-class users extends persons {
-  constructor(name: string, age: number) {
-    super(name, age);
+// Balls
+class ball {
+  x: number;
+  y: number;
+  velX: number;
+  velY: number;
+  color: string;
+  size: number;
+  constructor(
+    x: number, y: number, 
+    velX: number, velY: number,
+    color: string, size: number
+  ) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
-  bay() {
-    console.log(`bay ${this.name} of ${this.age} years`);
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
   }
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  collisionDetect() {
+    for (const boll of balls) {
+      if (!(this == boll)) {
+        const dx = this.x - boll.x;
+        const dy = this.y - boll.x;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.size + boll.size) {
+          boll.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+};
+
+//Animate balls
+const balls: any = [];
+
+while (balls.length < 5) {
+  const size = random(10, 20);
+  const boll = new ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  balls.push(boll);
 }
 
-const a: obj = new persons('jhosse', 22);
-const b: obj = new users('kevin', 20);
+const loop = () => {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (const boll of balls) {
+    boll.draw();
+    boll.update();
+    boll.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+
+
